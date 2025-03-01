2bd500c820725bb0a4ec320e56e9e3a23f1bd12e
Miguelsotoyurivilca <143229552+Miguelsotoyurivilca@users.noreply.github.com>
2025-02-28 23:41:23 -0500

    cambios

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import CartWidget from './CartWidget';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <h1>
+            <span className="logo-icon">☕</span> CaféMío
+          </h1>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="#" className="nav-link">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Productos</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Nosotros</a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">Contacto</a>
+          </li>
+        </ul>
+        <CartWidget />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
